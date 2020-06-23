11b227f242fbc4b737f21401901fab8b63c4ef6c
Антон Поляков <polyakov9.anton9@gmail.com>
2020-06-23 20:35:58 +0300

    test ws

--- test_web_server.py
+++ test_web_server.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
-
+import random
 app = Flask(__name__)
 
 
 @app.route('/tg', methods=['GET', 'POST'])
 def test():
     print(request)
-    return ''
+    return str(random.randint(0, 100))
 
 
 app.run(debug=True)
