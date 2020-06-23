0ac4b751c7d85537fd6e2c38615675c49f766bc6
Антон Поляков <polyakov9.anton9@gmail.com>
2020-06-23 19:21:18 +0300

    pre deploy

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from webshop.bot.main import start_bot
+from webshop.bot.main import start_bot, bot
+from webshop.bot import config
+from flask import Flask, request, abort
+
+from telebot.types import Update
+
+app = Flask(__name__)
+
+
+@app.route(config.WEBHOOK_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+    else:
+        abort(403)
+
 
 if __name__ == '__main__':
-    start_bot()
+    bot.set_webhook(
+        config.WEBHOOK_URL,
+        certificate=open('webhook_cert.pem', 'r')
+    )
 
